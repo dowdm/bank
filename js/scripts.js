64d6057ecb673479b3ec82238f84ef0cd66eaa0a
Matt Dowdney and Matt Miller <matt@>
2018-04-03 16:07:02 -0700

    refactor code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 //Business Logic
 var bankAccountArray = [];
+var numberGenerator = 0
 
 function BankAccount(userName, accountNumber, balance) {
   this.userName = userName;
   this.accountNumber = accountNumber;
   this.balance = balance;
-}
-
-BankAccount.prototype.balance = function() {
-  return this.balance;
 }
 
 BankAccount.prototype.deposit = function(number) {
@@ -23,12 +20,9 @@ BankAccount.prototype.withdrawal = function(number) {
   }
 }
 
-var numberGenerator = 0
-
 function depositWithdraw(accountId, deposit, withdrawal) {
   bankAccountArray.forEach(function(account){
     if (account.accountNumber === accountId) {
-      console.log("test");
       account.deposit(deposit);
       account.withdrawal(withdrawal);
     }
@@ -37,6 +31,7 @@ function depositWithdraw(accountId, deposit, withdrawal) {
 
 //User Logic
 $(function(){
+  //New Account Form
   $("#new-account").submit(function(event){
     var newAccountName = $("#new-account-name").val();
     var newAccountDeposit = parseInt($("#initial-deposit").val());
@@ -44,21 +39,18 @@ $(function(){
       (numberGenerator += 1);
     var account = new BankAccount(newAccountName,newAccountNumber, newAccountDeposit);
     bankAccountArray.push(account);
-/////////////////
     $("#account-number").text(newAccountNumber);
     $("#current-balance").text(newAccountDeposit);
     $(".no-display").show();
     event.preventDefault();
   });
-
-
-/////////
+  // Deposit/Withdraw Form
   $("#deposit-withdrawal").submit(function(event){
+    $("#no-money").hide();
     var accountNumberInput = parseInt($("#account-number-input").val());
     var userDeposit = parseInt($("#deposit").val());
     var userWithdrawal = parseInt($("#withdrawal").val());
     depositWithdraw(accountNumberInput, userDeposit, userWithdrawal);
-    console.log(bankAccountArray);
     bankAccountArray.forEach(function(account){
       if (account.accountNumber === accountNumberInput) {
         $("#current-balance").text(account.balance);
